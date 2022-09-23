ed455015f5811dfc4df6fc881da1acaffdb43b26
KyungMin <kgm7642@gmail.com>
2022-09-23 11:44:28 +0900

    FEAT : 네이버 로그인 구현

--- src/main/java/com/project/domain/Users.java
+++ src/main/java/com/project/domain/Users.java
@@ -3,17 +3,17 @@ package com.project.domain;
 import lombok.Data;
 
 @Data
-public class UsersJoin {
-
+public class Users {
 	private String usersnumber; //DB에서 PK 값
-	private String usersid; // 로그인용 ID 값
+	private String username; // 로그인용 ID 값
 	private String userspw; // 비밀번호
 	private String usersnickname; // 닉네임
 	private String usersemail; // 이메일
-    private boolean emailVerified;	//이메일 인증 여부
-	private boolean userslock; // 계정 잠김 여부
-	private String role; // 유저 역할
+	private String Role; // 유저 역할
 	private String createdate; // 계정 생성 날짜
 	private String modifydate; // 계정 수정 날짜
-	
+	private String provider; // google
+	private String providerid; // sub값
+	private String[] skillarry;	// 기술 스택
+	private String skill;
 }
